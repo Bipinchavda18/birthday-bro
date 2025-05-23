51b1f5420adef57d23b83d9d559262ac94f8b84c
mr.bipinchavda <mr.bipinchavda@gmail.com>
2025-05-23 13:00:15 +0000

    First commit

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    allowedDevOrigins: [
+      'http://localhost:3000',
+      'https://your-allowed-origin.com',
+    ],
+  };
+  
+  module.exports = nextConfig;
+  
